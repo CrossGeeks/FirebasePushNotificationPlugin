0339676e483c0f4cd7cd2a5f7bcd4b0afdc78f55
Rendy Del Rosario <rendy.delrosario@gmail.com>
2017-12-17 12:37:48 -0400

    Working with splash sample

---
+++ samples/FirebasePushSplashSample/FirebasePushSplashSample.Android/SplashActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FirebasePushSplashSample.Droid
+{
+    [Activity(Theme = "@style/MainTheme.Splash", MainLauncher = true, NoHistory = true, ScreenOrientation = global::Android.Content.PM.ScreenOrientation.Portrait)]
+
+    public class SplashActivity : AppCompatActivity
+    {
+
+        public override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+        }
+
+    }
+}
